d90a126d54b2955944fb9541a5ed9b9a5f6d9314
r3flex <r3flexmlg@gmail.com>
2020-06-03 12:42:27 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   get 'welcome/index'
  
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
+  resources :pages do
+  end
+
+  #get 'stypa/tv', :to => 'pages#index'
 
   root 'welcome#index'
 end
